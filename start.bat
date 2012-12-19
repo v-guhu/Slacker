1ac9ba533720ebbe8cbaaccfc4783e075f3465f2
xausee <xausee@gmail.com>
2012-12-19 10:04:13 +0800

    modify start bat
    modify start bat

--- start.bat
+++ start.bat
@@ -1,4 +1,7 @@
 @echo off
+color 0e
+mode con: cols=50 lines=25
+title BugCountTool
 prompt BugCountTool
 echo Thank you for using bug count tool, any suggestion or question please mail me: xausee@gmail.com
 echo This is a GUI version, it's easy to manipulate it.
